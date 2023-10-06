63e08af5881ec92614276641a46d12927adf3ade
ffcc999999 <119822454+ffcc999999@users.noreply.github.com>
2023-10-06 23:16:09 +0200

    Update pterodactyl.sh

--- pterodactyl.sh
+++ pterodactyl.sh
@@ -7,8 +7,8 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyr
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 apt update -y
 apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server wget sudo
-COMPOSER_ALLOW_SUPERUSER=1
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+export COMPOSER_ALLOW_SUPERUSER=1
+COMPOSER_ALLOW_SUPERUSER=1 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 mkdir -p /var/www/pterodactyl
 cd /var/www/pterodactyl
 curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
@@ -17,8 +17,8 @@ chmod -R 755 storage/* bootstrap/cache/
 mysqlpass=`openssl rand -base64 16`
 echo "CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '$mysqlpass'; CREATE DATABASE panel; GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;" | mysql -u root
 cp .env.example .env
-COMPOSER_ALLOW_SUPERUSER=1
-composer install --no-dev --optimize-autoloader --no-interaction
+export COMPOSER_ALLOW_SUPERUSER=1
+COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader --no-interaction
 php artisan key:generate --force
 sed -i 's/^DB_PASSWORD.*$/DB_PASSWORD=$mysqlpass/' .env
 sed -i 's/^APP_URL.*$/APP_URL=http://$hostname/' .env
